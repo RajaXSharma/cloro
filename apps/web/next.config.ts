047acb0add4543397d93534a0fffe8f3822ac946
rajaXSharma <rajasharma844891@gmail.com>
2026-09-21 18:52:40 +0530

    fix console logs showing in website

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   transpilePackages: ["shared"],
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
+  },
 };
 
 export default nextConfig;
